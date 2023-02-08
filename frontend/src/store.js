701443f12c719a3a9b81c643055334a50e09e771
mauro404 <mau.mansano@gmail.com>
2023-02-08 13:48:58 +0100

    create order constants, reducer and actions

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,9 +2,10 @@ import { legacy_createStore as createStore, combineReducers, applyMiddleware } f
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension'
 
-import { productsReducer, productDetailsReducer } from './reducers/productReducers';
+import { productsReducer, productDetailsReducer, newReviewReducer } from './reducers/productReducers';
 import { authReducer, userReducer, forgotPasswordReducer } from './reducers/userReducers';
 import { cartReducer } from './reducers/cartReducers';
+import { newOrderReducer, myOrdersReducer, orderDetailsReducer } from './reducers/orderReducers';
 
 const reducers = combineReducers({
     products: productsReducer,
@@ -12,7 +13,11 @@ const reducers = combineReducers({
     auth: authReducer,
     user: userReducer,
     forgotPassword: forgotPasswordReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    newOrder: newOrderReducer,
+    myOrders: myOrdersReducer,
+    orderDetails: orderDetailsReducer,
+    newReview: newReviewReducer
 });
 
 let initialState = {
