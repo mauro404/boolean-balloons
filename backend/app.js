410d1c3e762e523ce6335a0fc3c23f214e36c4f1
mauro404 <mau.mansano@gmail.com>
2023-03-07 10:16:52 +0100

    update backend to handle larger pictures files

--- backend/app.js
+++ backend/app.js
@@ -8,8 +8,8 @@ const dotenv = require('dotenv');
 dotenv.config({ path: 'backend/config/config.env' })
 const errorMiddleware = require('./middlewares/errors');
 
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(fileUpload());
 
