39006c070f9943805117d2db89b453b73e885dcb
mauro404 <mau.mansano@gmail.com>
2023-03-08 16:24:15 +0100

    update cart styles and responsiveness

--- frontend/src/components/cart/Cart.js
+++ frontend/src/components/cart/Cart.js
@@ -1,117 +1,168 @@
-import React, { Fragment } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { addItemToCart, removeItemFromCart } from '../../actions/cartActions'
-import MetaData from '../layout/MetaData'
-import { Button, Col, Row, Container, Stack } from 'react-bootstrap'
+import React, { Fragment } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { addItemToCart, removeItemFromCart } from "../../actions/cartActions";
+import MetaData from "../layout/MetaData";
+import { Button, Col, Row, Container, Stack } from "react-bootstrap";
 
 const Cart = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const { cartItems } = useSelector(state => state.cart)
+  const { cartItems } = useSelector((state) => state.cart);
 
-    const removeCartItemHandler = (id) => {
-        dispatch(removeItemFromCart(id))
-    }
+  const removeCartItemHandler = (id) => {
+    dispatch(removeItemFromCart(id));
+  };
 
-    const increaseQty = (id, quantity, stock) => {
-        const newQty = quantity + 1;
+  const increaseQty = (id, quantity, stock) => {
+    const newQty = quantity + 1;
 
-        if (newQty > stock) return;
+    if (newQty > stock) return;
 
-        dispatch(addItemToCart(id, newQty))
-    }
+    dispatch(addItemToCart(id, newQty));
+  };
 
-    const decreaseQty = (id, quantity) => {
+  const decreaseQty = (id, quantity) => {
+    const newQty = quantity - 1;
 
-        const newQty = quantity - 1;
+    if (newQty <= 0) return;
 
-        if (newQty <= 0) return;
+    dispatch(addItemToCart(id, newQty));
+  };
 
-        dispatch(addItemToCart(id, newQty))
+  const checkoutHandler = () => {
+    navigate("/shipping");
+  };
 
-    }
+  return (
+    <Fragment>
+      <MetaData title={"Shopping Cart"} />
+      {cartItems.length === 0 ? (
+        <Container className="center">
+          <div>
+            <img
+              className="mx-auto d-block pb-2"
+              src="../../images/logo2.png"
+              alt="logo"
+              width="66px"
+            />
+            <h3>Your cart is Empty</h3>
+          </div>
+        </Container>
+      ) : (
+        <Container>
+          <h1 className="h3 mb-3 fw-normal mt-4">Your Cart</h1>
+          <hr className="mt-3" />
 
-    const checkoutHandler = () => {
-        navigate('/shipping')
-    }
+          {cartItems.map((item) => (
+            <Row
+              className="d-flex justify-content-between align-items-center"
+              key={item.product}
+            >
+              <Col md={6} className="d-flex">
+                <div className="">
+                  <img
+                    className="rounded-3"
+                    src={item.image}
+                    alt={item.name}
+                    height=""
+                    width="194"
+                  />
+                </div>
+                <div className="align-self-center p-3">
+                  <h6 className="">
+                    <Link
+                      style={{ textDecoration: "none", color: "black" }}
+                      to={`/product/${item.product}`}
+                    >
+                      {item.name}
+                    </Link>
+                  </h6>
+                  <h6
+                    className="text-muted text-black mb-0"
+                    style={{ fontSize: ".75rem" }}
+                  >
+                    Unit price: ${item.price.toFixed(2)}
+                  </h6>
+                </div>
+              </Col>
 
-    return (
-        <Fragment>
-            <MetaData title={'Shopping Cart'} />
-            {cartItems.length === 0 ? <Container className="LoginPage">
-                <img
-                    className="mb-4"
-                    src="../../images/logo2.png"
-                    alt="logo"
-                    width="66px"
-                />
-                <h1 className="h3 mb-3 fw-normal">Your cart is Empty</h1>
-                </Container> : (
-                <Container>
-                    <h1 className="h3 mb-3 fw-normal mt-4">Your Cart</h1>
-                    <hr className="mt-3"/>
+              <Col md={6}>
+                <div>
+                  <Stack
+                    direction="horizontal"
+                    className="d-flex justify-content-md-end justify-content-sm-between"
+                  >
+                    <Col className="col-4 col-lg-3 d-flex flex-row">
+                      <span
+                        className="btn btn-light minus"
+                        onClick={() => decreaseQty(item.product, item.quantity)}
+                      >
+                        -
+                      </span>
+                      <input
+                        type="number"
+                        className="form-control count text-center"
+                        value={item.quantity}
+                        readOnly
+                      />
+                      <span
+                        className="btn btn-light plus"
+                        onClick={() =>
+                          increaseQty(item.product, item.quantity, item.stock)
+                        }
+                      >
+                        +
+                      </span>
+                    </Col>
+                    <Col className="col-5 col-md-1"></Col>
+                    <span className="" style={{ color: "#0D6EFD" }}>
+                      ${(item.price * item.quantity).toFixed(2)}
+                    </span>
+                    <Col className="col-1"></Col>
+                    <div className="">
+                      <i
+                        id="delete_cart_item"
+                        className="fa fa-trash"
+                        onClick={() => removeCartItemHandler(item.product)}
+                      ></i>
+                    </div>
+                  </Stack>
+                </div>
+              </Col>
 
-                    {cartItems.map(item => (
-                        <Row className="d-flex justify-content-between align-items-center" key={item.product}>
-                            <Col md="2" lg="2" xl="2">
-                                <img className="rounded-3"src={item.image} alt={item.name} height="" width="194" />
-                            </Col>
-                            <Col md="3" lg="3" xl="3">
-                                <h6><Link style={{ textDecoration: "none", color: "black" }} to={`/product/${item.product}`}>{item.name}</Link></h6>
-                                <h6 className="text-muted text-black mb-0" style={{ fontSize: ".75rem" }}>
-                                    Unit price: ${item.price.toFixed(2)}
-                                </h6>
-                            </Col>
+              <hr className="mt-3" />
+            </Row>
+          ))}
 
-                            <Col className="col-12 col-lg-6 d-flex">
-                                <Col className="col-6 col-lg-9">
-                                    <div></div>
-                                </Col>
-                                <Col className="col-6 col-lg-4">
-                                    <Stack direction='horizontal' className=''>
-                                        <span className="btn btn-light minus" onClick={() => decreaseQty(item.product, item.quantity)}>-</span>
-                                        <input type="number" className="form-control count text-center" value={item.quantity} readOnly />
-                                        <span className="btn btn-light plus" onClick={() => increaseQty(item.product, item.quantity, item.stock)}>+</span>
-                                        <Col className="col-1 col-lg-2">   
-                                        </Col>
-                                        <span className="" style={{ color: "#0D6EFD" }}>
-                                        ${((item.price)*(item.quantity)).toFixed(2)}
-                                        </span>
-                                    </Stack>
-                                </Col>
+          <Row className="text-end mb-4 justify-content-end">
+            <p>
+              {" "}
+              Items Price:{" "}
+              <span>
+                $
+                {cartItems
+                  .reduce((acc, item) => acc + item.quantity * item.price, 0)
+                  .toFixed(2)}
+              </span>
+            </p>
 
-                                <Col className="col-1 col-lg-1">   
-                                </Col>
-                            </Col>
+            <hr />
+            <Col md="2" lg="2" xl="2">
+              <Button
+                id="checkout_btn"
+                className="btn btn-lg"
+                onClick={checkoutHandler}
+              >
+                Check out
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+      )}
+    </Fragment>
+  );
+};
 
-                            <Col className="col-1 col-lg-1 text-end">
-                                <i id="delete_cart_item" className="fa fa-trash" onClick={() => removeCartItemHandler(item.product)} ></i>
-                            </Col>
-
-                            <hr className="mt-3"/>
-                        </Row>
-                    ))}
-
-                        <Row className="text-end mb-4 justify-content-end">
-                                <p> Items Price:{" "}
-                                    <span>
-                                        ${cartItems.reduce((acc, item) => acc + item.quantity * item.price, 0).toFixed(2)}
-                                    </span>
-                                </p>
-
-                                <hr />
-                                <Col md="2" lg="2" xl="2">
-                                <Button id="checkout_btn" className="btn btn-lg" onClick={checkoutHandler}>
-                                    Check out
-                                </Button>
-                                </Col>
-                        </Row>
-                </Container>
-            )}
-        </Fragment>
-    )
-}
-
-export default Cart
+export default Cart;
