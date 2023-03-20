36c7d096f33a2c730bb65ac2a464faedae6fc978
mauro404 <mau.mansano@gmail.com>
2023-03-20 17:15:08 +0100

    update about page component

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,14 +1,19 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import MetaData from "./layout/MetaData";
+
+import { Container, Row, Col } from "react-bootstrap";
 
 function About() {
   return (
-    <section id="about" className="">
-      <Container className="py-5 fw-semibold h-100 w-75 text-center">
-        <div className="">
-        <h1 className="py-lg-5 text-center">
-            About
-        </h1>
+    <section id="" className="">
+      <MetaData title={"About"} />
+
+      <Container className="py-lg-5 mt-3 px-lg-5 row d-flex justify-content-center mx-auto">
+        <div className="col-10 col-lg-6">
+        <img className="rounded-3 img-fluid" src="../../images/about_logo.png" alt=""/>
+        </div>
+        <div className="col-10 col-lg-6 mt-3 mt-lg-0">
+        <h1>About</h1>
         <p>
           Welcome to boolean-balloons online store, where we specialize in
           selling high-quality balloons for all occasions!
@@ -23,28 +28,28 @@ function About() {
           occasion.
         </p>
 
-        <p>
+        {/* <p>
           At our store, we pride ourselves on providing the highest quality
           balloons at the most affordable prices. We understand that our
           customers have a variety of needs and preferences, which is why we
           offer a wide range of balloon types, colors, shapes, and sizes to
           choose from.
-        </p>
+        </p> */}
 
-        <p>
+        {/* <p>
           We believe that everyone deserves to celebrate their special moments
           in a way that's both fun and responsible. That's why we are committed
           to using eco-friendly materials in our balloon products wherever
           possible.
-        </p>
+        </p> */}
 
         <p>
           Our team is dedicated to providing excellent customer service, and we
           are always available to answer any questions you may have about our
           products or services.
-        </p>
+        {/* </p>
 
-        <p>
+        <p> */}
           We invite you to browse our online store and discover the joy of
           balloons for yourself. Thank you for choosing us as your go-to source
           for all your balloon needs!
